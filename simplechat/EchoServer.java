5a7b9502ba2c05f91111f089cba7c7b3fba272c7
ZhenHaoWu <zwu097@0110S056.site.uottawa.ca>
2014-11-23 19:16:00 -0500

    hello

--- simplechat/EchoServer.java
+++ simplechat/EchoServer.java
@@ -49,13 +49,14 @@ public class EchoServer extends AbstractServer
    */
   public void handleMessageFromClient(Object msg)
   {
-    if (c=false){
+    if (d=false && dcount<1 && pcount<1){
       if (msg instanceof Operative) {
         for(a<length(currOperatives)){
           if(currOperatives<a> == msg){
-            client.setInfo("String",msg.aCodeName);
-            this.sendToAllClients(client.getInfo("String") + " has been connected");
-            c=true;
+            Operative client=msg;
+            this.sendToAllClients(client.aCodeName + " has been connected");
+            d=true;
+            dcount++;
           }
           else if(a<(length(currOperatives)-1)){
           a++;
@@ -63,56 +64,49 @@ public class EchoServer extends AbstractServer
           else {
             for(b<length(privateOperatives)){
               if(privateOperatives<b> == msg){
-                client.setInfo("String",msg.aCodeName);
-                this.sendToAllClients(client.getInfo("String") + " has been connected");
-                c=true;
+                Operative client=msg;
+                this.sendToAllClients('waring');
+                d=true;
+                pcount++;
               }
-              else
-              {
+              else if(b<(length(privateOperatives)-1)){
                 b++;
               }
-            }
+              for(c<length(missionOperatives)){
+                  if(missionOperatives<c> == msg && mcount<1){
+                    client.setInfo("String",msg.aCodeName);
+                    System.out.println(client.getInfo("String") + " has completed the mission");
+                    setDymaticPassword(msg);
+                    int pa = getDymaticPassword(msg);
+                    System.out.println("yout new dymatic password will be" + pa);
+                    quit();
+                  }
+                  else if(missionOperatives<c> == msg && mcount>1){
+                    System.out.println("warning");
+                    quit();
+                  }
+                  else{
+                  c++; 
+                  }}}}}}
       else 
       {
         System.out.println("Server has stopped listening for connections.");
-      }}}}}
+      }}
       else{
         if (msg instanceof Mission) {
           Mission.add(msg);
         }
-        if (msg instanceof String) {
-      if (msg.equals("#getmission")){
-        for(a<length(currOperatives)){
-          if(currOperatives<a> == msg){
-            client.setInfo("String",msg.aCodeName);
-            this.sendToAllClients(client.getInfo("String") + " has been connected");
-            c=true;
-          }
-          else if(a<(length(currOperatives)-1)){
-          a++;
+        else if (msg instanceof String) {
+          if (msg.toString().startsWith("#getmission")){
+            for(e<length(checklist)){
+              if(client.aCodeName==checklist<e>.Operative.aCodeName){
+                Mission a=checklist<e>.Mission;
+                System.out.println("aAssignment Date"+a.aAssignmentDate + "EndDate", +a.aEndDate + "description" + a.aDescription)
+              }
+        }  
           }
       }
-    }
-    if(A.a)
-    {
-      A t = msg.toString();
-      String login = t.subString(6);
-      String password=login.subString(8);
-      if(login==login.setID() && password==login.getpassword()){
-      logn.trim();
-      client.setInfo("String",logn);
-      this.sendToAllClients(client.getInfo("String") + " has been connected");
-      }
-    }
-    System.out.println("Message received: " + msg + " from " + client);
-    this.sendToAllClients(msg);
-  }
-    else
-    {
-      System.out.println("Message received: " + msg + " from " + client.getInfo("String"));
-      this.sendToAllClients(client.getInfo("String") + ": " + msg);
-    }
-    
+    } 
   /**
    * This method overrides the one in the superclass.  Called
    * when the server starts listening for connections.
@@ -132,6 +126,53 @@ public class EchoServer extends AbstractServer
     System.out.println
       ("Server has stopped listening for connections.");
   }
+
+  protected void setDymaticPassword(currOperative P){
+    Random rnd = new Random();
+    int len=8;
+    char[] pswd = new char[len];
+    int in = 0;
+    for (int i = 0; i < 4; i++) {
+      in = getNextIndex(rnd, len, pswd);
+      pswd[in] = NUM.charAt(rnd.nextInt(NUM.length()));
+    }
+    for(int i = 0; i < len; i++) {
+      if(pswd[i] == 0) {
+        pswd[i] = ALPHA.charAt(rnd.nextInt(ALPHA.length()));
+      }
+    }
+    P.password=pswd;
+  }
+    protected void setPrivatePassword(privateOperative P){
+    Random rnd = new Random();
+    int len=8;
+    char[] pswd = new char[len];
+    int in = 0;
+    for (int i = 0; i < 4; i++) {
+      in = getNextIndex(rnd, len, pswd);
+      pswd[in] = NUM.charAt(rnd.nextInt(NUM.length()));
+    }
+    for(int i = 0; i < len; i++) {
+      if(pswd[i] == 0) {
+        pswd[i] = ALPHA.charAt(rnd.nextInt(ALPHA.length()));
+      }
+    }
+    P.password=pswd;
+  }
+    private static int getNextIndex(Random rnd, int len, char[] pswd) {
+    int index = rnd.nextInt(len);
+    while(pswd[index = rnd.nextInt(len)] != 0);
+    return index;
+  }
+    public int getDymaticPassword(currOperative P){
+      return P.password;
+    }
+     public int getPrivatePassword(privateOperative P){
+      return P.password;
+    } 
+    public int getMisssionPassword(misssionOperative P){
+      return P.password;
+    }
   
   //Class methods ***************************************************
   
@@ -146,8 +187,15 @@ public class EchoServer extends AbstractServer
   {
     int port = 0; //Port to listen on
     int a = 0;
-    int b= 0;
+    int b = 0;
+    int c = 0;
     boolean c=false;
+    private int dcount=0;
+    private int pcount=0;
+    private int mcount=0;
+    private static final String ALPHA   = "abcdefghijklmnopqrstuvwxyz";
+    private static final String NUM   = "0123456789";
+
 
     ArrayList<Operative> currOperatives = new ArrayList();
 
@@ -163,6 +211,14 @@ public class EchoServer extends AbstractServer
     privateOperatives.add(new Operative("jimgdfgmmy","5435428"));
     privateOperatives.add(new Operative("jimfgfdgmmy","78637524"));
     privateOperatives.add(new Operative("jimfdgmmy","57324257"));
+
+    ArrayList<Operative> missionOperatives = new ArrayList();
+
+    missionOperatives.add(new Operative("jimmmy","sdfgfdsg"));
+    missionOperatives.add(new Operative("jimgdfgmmy","sdfgfdsg"));
+    missionOperatives.add(new Operative("jimfgfdgmmy","sdfgfds"));
+    missionOperatives.add(new Operative("jimfdgmmy","57gfh7"));
+
 
     try
     {
